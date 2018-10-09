a4926a2ad86dc1a7336976e336e3f6e6e5f03500
Eric Suchyta <eric.d.suchyta@gmail.com>
2018-10-09 14:59:03 +0000

    Bug fixes in replacement substitution during job setup

--- src/kittie.py
+++ src/kittie.py
@@ -22,6 +22,34 @@ import subprocess
 import sys
 import yaml
 
+import collections
+from collections import OrderedDict
+
+"""
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+"""
+
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+def dict_representer(dumper, data):
+    #return dumper.represent_dict(data.iteritems())
+    return dumper.represent_dict(data.items())
+
+def dict_constructor(loader, node):
+    return collections.OrderedDict(loader.construct_pairs(node))
+
+yaml.add_representer(collections.OrderedDict, dict_representer)
+yaml.add_constructor(_mapping_tag, dict_constructor)
+
 
 def KeepLinksCopy(inpath, outpath):
     if os.path.isdir(inpath):
@@ -202,7 +230,7 @@ class KittieJob(cheetah.Campaign):
     """
 
 
-    def _MakeReplacements(self, searchstr=None):
+    def _MakeReplacements(self, searchstr=None, main=None):
         """
         Look for ${} things to be replaced in the user's input config file, and replace them with the values defined elsewhere in the file.
         """
@@ -217,15 +245,19 @@ class KittieJob(cheetah.Campaign):
             searchstr = yaml.dump(self.config, default_flow_style=False)
             main = True
         else:
-            main = False
+            if main is None:
+                main = False
 
 
         pattern = '(?<!\$)\$\{(.*)\}'
         search = re.compile(pattern)
         results = search.findall(searchstr)
 
-
         for match in results:
+
+            if main:
+                self.config = yaml.load(searchstr.replace('$${', '${'))
+
             origmatch = match
 
             # This assumes that lists always end the entries. That's probably OK, at least for now
@@ -256,10 +288,14 @@ class KittieJob(cheetah.Campaign):
             subsearch = re.compile(subpattern)
             searchstr = subsearch.sub(str(value), searchstr, count=1)
 
+
         # Propogate the changes back into the config dictionary
         if main:
             searchstr = searchstr.replace('$${', '${')
             self.config = yaml.load(searchstr)
+            results = search.findall(searchstr)
+            if len(results) > 0:
+                match = self._MakeReplacements()
         else:
             return searchstr
 
